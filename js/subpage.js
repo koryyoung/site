8e9a02966d229ffad3b61a3218751c0f79af5337
koryyoung <66496615+koryyoung@users.noreply.github.com>
2023-03-09 18:17:19 -0500

    Add files via upload

--- js/subpage.js
+++ js/subpage.js
@@ -11,11 +11,47 @@ subBtn.addEventListener("click", function () {
     }, 1000);
 });
 
-var imgs = document.querySelectorAll('#wsub-image-side img, #wsub-image-wide img');
-var full = document.getElementById("wsub-full");
 
-imgs.forEach(img => {
-    img.addEventListener('click', function () {
-        full.style.display = 'flex';
+function fullPic() {
+var images = document.querySelectorAll(".work-sub img");
+var full = document.getElementById("wsub-full");
+var gOneS = document.getElementById("g1-s");
+var gTwoS = document.getElementById("g2-s");
+var gThrS = document.getElementById("g3-s");
+var gFouS = document.getElementById("g4-s");
+var gOneL = document.getElementById("g1-l");
+var gTwoL = document.getElementById("g2-l");
+var gThrL = document.getElementById("g3-l");
+var gFouL = document.getElementById("g4-l");
+
+images.forEach(img => {
+    img.addEventListener("click", function () {
+        full.style.display = "flex";
     });
 });
+
+gOneS.addEventListener("click", function () {
+    gOneL.style.display = "flex"
+});
+
+gTwoS.addEventListener("click", function () {
+    gTwoL.style.display = "flex"
+});
+
+gThrS.addEventListener("click", function () {
+    gThrL.style.display = "flex"
+});
+
+gFouS.addEventListener("click", function () {
+    gFouL.style.display = "flex"
+});
+
+full.addEventListener("click", function () {
+    full.style.display = "none"
+    gOneL.style.display = "none"
+    gTwoL.style.display = "none"
+    gThrL.style.display = "none"
+    gFouL.style.display = "none"
+});
+
+}
